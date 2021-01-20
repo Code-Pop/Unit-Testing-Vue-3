60d43a55a865c043fe3bb87f75e5f4b20d3babdc
Andy <andy@Andys-MacBook-Pro.local>
2021-01-20 11:25:33 -0500

    starting

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app-header">
+    <button v-show="loggedIn">Logout</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loggedIn: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.app-header {
+  min-height: 40px;
+  padding: 0.2em 1em;
+  background: white;
+}
+button {
+  float: right;
+}
+</style>
