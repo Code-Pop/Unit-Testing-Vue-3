baac4acc71fef16159c1dfe0520c28de99dacc81
Andy <andy@Andys-MacBook-Pro.local>
2021-01-20 23:39:24 -0500

    final

--- tests/unit/MessageContainer.spec.js
+++ tests/unit/MessageContainer.spec.js
@@ -3,6 +3,18 @@ import { mount } from '@vue/test-utils'
 
 describe('MessageContainer', () => {
   it('Wraps MessageDisplay component', () => {
-    const wrapper = mount(MessageContainer)
+    const wrapper = mount(MessageContainer, {
+      global: {
+        stubs: {
+          MessageDisplay: {
+            template: '<p data-testid="message">Hello from the db!</p>'
+          }
+        }
+      }
+    })
+
+    const stubMessage = 'Hello from the db!'
+    const message = wrapper.find('[data-testid="message"]').text()
+    expect(message).toEqual(stubMessage)
   })
 })
