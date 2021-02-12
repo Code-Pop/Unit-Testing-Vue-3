f1bfa99b48a79274d93ee2ba70b9a1085a65c73b
tutorialcode <63885381+tutorialcode@users.noreply.github.com>
2021-02-12 03:22:30 -0500

    move beforeEach

--- tests/unit/MessageDisplay.spec.js
+++ tests/unit/MessageDisplay.spec.js
@@ -5,11 +5,11 @@ import flushPromises from 'flush-promises'
 
 jest.mock('@/services/axios')
 
-describe('MessageDisplay', () => {
-  beforeEach(() => {
-    jest.clearAllMocks()
-  })
+beforeEach(() => {
+  jest.clearAllMocks()
+})
 
+describe('MessageDisplay', () => {
   it('Calls getMessage once and displays message', async () => {
     const mockMessage = 'Hello from the db'
     getMessage.mockResolvedValueOnce({ text: mockMessage })
